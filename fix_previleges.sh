9a7c3176872efce952a0419559df8a70fcc48e48
yashwanthiit2009 <YeswanthKumar_Pagadala@student.uml.edu>
2019-09-07 23:10:46 -0400

    Update fix_previleges.sh

--- fix_previleges.sh
+++ fix_previleges.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod +x /home/ec2-user/Sample-0.0.1-SNAPSHOT.jar
+chmod +x /home/ec2-user/gameoflife-0.0.1-SNAPSHOT.jar
 chmod +x /home/ec2-user/start_server.sh
 chmod +x /home/ec2-user/stop_server.sh
 
